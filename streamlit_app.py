f53d3d14dd7cf97cbdc78778aa5237e6695d9d84
Mikialanam <miki.alanam@gmail.com>
2024-07-31 23:29:27 +0000

    A

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 from openai import OpenAI
 
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 # Show title and description.
 st.title("📄 Yee haw Document question answering")
 st.write(
