623782ccc16e59be8141b17486bc33a4f46fae77
Mikialanam <miki.alanam@gmail.com>
2024-08-01 01:42:48 +0000

    DF

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,13 @@ hide_st_style = """
             .viewerBadge_text__fzr3E {visibility: hidden;}
             .viewerBadge_link__qRIco {visibility: hidden;}
             .viewerBadge_container__r5tak.styles_viewerBadge__CvC9N {visibility: hidden;}
+            
+            .block-container
+                {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    margin-top: 1rem;
+                }
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
