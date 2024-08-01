95c476712b5ff4f53189d5b45e76da3fcf59f7ba
Mikialanam <miki.alanam@gmail.com>
2024-08-01 00:09:02 +0000

    adfsadf

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,10 @@ hide_st_style = """
             .styles_stateContainer__CelYF {visibility: hidden;}
             .viewerBadge_text__fzr3E {visibility: hidden;}
             .viewerBadge_link__qRIco {visibility: hidden;}
-            .viewerBadge_container__r5tak styles_viewerBadge__CvC9N {visibility: hidden;}
+            .viewerBadge_container__r5tak.styles_viewerBadge__CvC9N {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # Show title and description.
-st.title("📄 Yee haw Update 3")
+st.title("📄 Yee haw Update 4")
