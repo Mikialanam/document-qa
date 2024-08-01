644a66ec65b79cea89d71d6c79362ce387ab8215
Mikialanam <miki.alanam@gmail.com>
2024-08-01 00:01:56 +0000

    asdff

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ hide_st_style = """
             footer {visibility: hidden;}
             header {visibility: hidden;}
 
-            .viewerBadge_link__qRIco {visibility: hidden;}
+            .viewerBadge_container__r5tak styles_viewerBadge__CvC9N {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # Show title and description.
-st.title("📄 Yee haw Document question answering")
+st.title("📄 Yee haw Update 1")
